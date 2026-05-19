071345de2d0bf56e13d772ece1900dadaa716f3a
JAQUELINE DE OLIVEIRA VIANA <jaqueline.viana+bradesco@bradesco.com.br>
2026-05-19 15:33:18 -0300

    Teste

--- src/main/ClassePrincipal.java
+++ src/main/ClassePrincipal.java
@@ -1,7 +1,7 @@
 package main;
 
 public class ClassePrincipal {
-    static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
     }
 }
