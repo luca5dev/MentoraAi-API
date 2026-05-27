8ed9f9a28f3499e5d27b974b38af7d622b9e3ed1
JAQUELINE DE OLIVEIRA VIANA <jaqueline.viana+bradesco@bradesco.com.br>
2026-05-27 17:16:06 -0300

    Alterações no ConsuleInput após code review

--- src/main/java/app/util/ConsoleInput.java
+++ src/main/java/app/util/ConsoleInput.java
@@ -2,6 +2,8 @@ package app.util;
 
 import app.exception.CampoVazioException;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 //Classe de leitura de texto
@@ -9,40 +11,58 @@ public class ConsoleInput {
 
     public static final Scanner sc = new Scanner(System.in);
 
-    public static int lerNumero(){
-        int numero = sc.nextInt();
-        sc.nextLine();
-        return numero;
+    private ConsoleInput() {
+        throw new UnsupportedOperationException("Classe utilitária não pode ser instanciada");
     }
 
-    public static Double lerValor(){
-        Double valor = sc.nextDouble();
-        sc.nextLine();
-        return valor;
-    }
+    public static int lerNumero() {
+        try {
+            int numero = sc.nextInt();
+            limpaBuffer();
+            return numero;
 
-    public static char lerOpcao(){
-        char opc = sc.next().charAt(0);
-        return opc;
-    }
-
-    public static String lerTexto(){
-        String entrada = sc.nextLine().trim();
-
-        if (entrada.isEmpty()){
-            throw new CampoVazioException("Este campo não pode estar vazio");
-        } else {
-            return entrada;
+        } catch (InputMismatchException e) {
+            limpaBuffer();
+            throw new CampoVazioException("Entrada inválida. Por favor, insira um número.");
         }
     }
 
-    public static String limpaBuffer(){
-        return sc.nextLine();
+    public static Double lerValor() {
+        try {
+            Double valor = sc.nextDouble();
+            limpaBuffer();
+            return valor;
+        } catch (InputMismatchException e) {
+            limpaBuffer();
+            throw new CampoVazioException("Entrada inválida. Por favor, insira um valor numérico.");
+        }
+    }
+
+    public static char lerOpcao() {
+        try {
+            char opcao = sc.next().charAt(0);
+            limpaBuffer();
+            return opcao;
+        } catch (NoSuchElementException | IllegalStateException e) {
+            limpaBuffer();
+            throw new CampoVazioException("Entrada inválida. Por favor, insira um caractere válido.");
+        }
+    }
+
+    public static String lerTexto() {
+            String entrada = sc.nextLine().trim();
+
+            if (entrada.isEmpty()) {
+                throw new CampoVazioException("Este campo não pode estar vazio");
+            }
+            return entrada;
+    }
+
+    public static void limpaBuffer() {
+        sc.nextLine();
     }
 
     public static void fecharScanner(){
         sc.close();
     }
-
-
 }
